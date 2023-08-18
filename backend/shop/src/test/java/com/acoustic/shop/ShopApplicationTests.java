dc9d655c889355204ea134b2a09b0e2b3bccfbad
Heeseok Lee <2dilution2@gmail.com>
2023-08-18 17:07:17 +0900

    gradle로 리빌드

--- backend/shop/src/test/java/com/acoustic/shop/ShopApplicationTests.java
+++ backend/shop/src/test/java/com/acoustic/shop/ShopApplicationTests.java
@@ -1,4 +1,4 @@
-package acoustic.shop;
+package com.acoustic.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
